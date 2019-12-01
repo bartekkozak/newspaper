4d80571c1b9777fd8e7e1b77d7a223eaf84a15b1
bartlomiejkozak <bartlomiej.kozak@redvike.com>
2019-12-01 19:18:00 +0100

    Create News component

--- src/News/News.tsx
+++ src/News/News.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { selectAllNews } from '../Store/Reducers/newsReducer'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  newsSelector,
+  selectFashionLoading,
+  selectSportsLoading,
+  selectSportsError,
+  selectFashionError,
+} from '../Store/Reducers/newsReducer'
 
-const News = () => {
-  const news = useSelector(selectAllNews)
+import { getFashionNewsData, getSportsNewsData } from '../Store/Actions/newsActions'
+import Article from './Article/Article'
+import styles from './News.module.css'
+import ErrorMessage from './ErrorMessage/ErrorMessage'
+import { AppState } from '../Utils/interfaces/interfaces'
 
-  return <div data-testid="news"></div>
+const News: React.FC = () => {
+  const dispatch = useDispatch()
+  const { news, fashionLoading, fashionError, sportsLoading, sportsError } = useSelector((state: AppState) => ({
+    news: newsSelector(state),
+    fashionLoading: selectFashionLoading(state),
+    fashionError: selectFashionError(state),
+    sportsLoading: selectSportsLoading(state),
+    sportsError: selectSportsError(state),
+  }))
+
+  useEffect(() => {
+    dispatch(getFashionNewsData())
+    dispatch(getSportsNewsData())
+  }, [])
+
+  return (
+    <div data-testid="news" className={styles.container}>
+      {fashionError && (
+        <ErrorMessage category="fashion" loading={fashionLoading} onClick={() => dispatch(getFashionNewsData())} />
+      )}
+      {sportsError && (
+        <ErrorMessage category="sports" loading={sportsLoading} onClick={() => dispatch(getSportsNewsData())} />
+      )}
+      {fashionLoading && sportsLoading && <p>Loading...</p>}
+      {news.length > 0 && news.map(article => <Article article={article} key={article.id} />)}
+    </div>
+  )
 }
 
 export default News
