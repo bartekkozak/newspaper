67850ee6c32b0f6ec1d69b792beffba942d462f1
bartlomiejkozak <bartlomiej.kozak@redvike.com>
2019-12-01 19:17:06 +0100

    Refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React, { Component, ReactNode } from 'react'
 import { Provider } from 'react-redux'
 import { Store } from 'redux'
-import News from './News/News'
+import Layout from './Layout/Layout'
 
 class App extends Component<{ store: Store }> {
   render(): ReactNode {
     return (
       <Provider store={this.props.store}>
-        <News />
+        <Layout />
       </Provider>
     )
   }
